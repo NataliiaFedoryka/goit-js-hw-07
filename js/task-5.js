a1d4728f4e8bad68f8495590f4bc9c2d9203430b
Nataliia Fedoryka <fedorykanataliya@gmail.com>
2024-05-26 21:50:58 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,9 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+document.querySelector(`.change-color`).addEventListener(`click`, () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  document.querySelector(`.color`).textContent = randomColor;
+}
+);
