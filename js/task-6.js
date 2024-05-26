9ee08ce73c558497042c0f7c551b21af9756a5d5
Nataliia Fedoryka <fedorykanataliya@gmail.com>
2024-05-26 22:53:05 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector(`#controls input`);
+const createBtn = document.querySelector(`[data-create]`);
+const destroyBtn =  document.querySelector(`[data-destroy]`);
+const boxesContainer = document.getElementById(`boxes`);
+
+createBtn.addEventListener(`click`, () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = ``;
+  }
+  else {
+    alert (`Please enter a number between 1 and 100.`);
+  }
+  destroyBtn.addEventListener(`click`, destroyBoxes);
+  function createBoxes(amount) {
+    destroyBoxes();
+    let size =30;
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement(`div`);
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxesContainer.appendChild(box);
+      size += 10;
+    }
+  }
+  function destroyBoxes() {
+    boxesContainer.innerHTML = ``;
+  }
+}
+);
