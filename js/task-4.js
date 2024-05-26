85220a47f9c57992b76ba87ae1c290ebac5d1b91
Nataliia Fedoryka <fedorykanataliya@gmail.com>
2024-05-26 17:30:57 +0300

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
 
+document.querySelector(`.login-form`).addEventListener(`submit`, (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get(`email`).trim;
+    const password = formData.get(`password`).trim;
+    if(!email || !password) {
+      return  alert (`All form fields must be filled in`)
+    }
+    const formObject = {
+        email: email,
+        password: password,
+    };
+    console.log(formObject);
+    e.target.removeEventListener();
+} );
