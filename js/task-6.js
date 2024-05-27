f6dd7450bd6714198888158e294dafd1b1feace6
Nataliia Fedoryka <fedorykanataliya@gmail.com>
2024-05-27 14:02:24 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,35 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const input = document.querySelector(`#controls input`);
-const createBtn = document.querySelector(`[data-create]`);
-const destroyBtn =  document.querySelector(`[data-destroy]`);
+const controls = document.getElementById(`controls`);
+const input = document.querySelector(`input`);
+const createBtn = controls.querySelector(`[data-create]`);
+const destroyBtn = controls.querySelector(`[data-destroy]`);
 const boxesContainer = document.getElementById(`boxes`);
-
-createBtn.addEventListener(`click`, () => {
-  const amount = parseInt(input.value);
-  if (amount >= 1 && amount <= 100) {
+function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+  let size = 30;
+  for (let i = 0; i<amount; i++) {
+    const div = document.createElement(`div`);
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(div);
+    size +=10;
+  }
+boxesContainer.innerHTML = ``;
+boxesContainer.appendChild(fragment);}
+ function destroyBoxes() {
+  boxesContainer.innerHTML = ``;
+ }
+createBtn/addEventListener('click', () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100){
     createBoxes(amount);
     input.value = ``;
   }
-  else {
-    alert (`Please enter a number between 1 and 100.`);
-  }
-  destroyBtn.addEventListener(`click`, destroyBoxes);
-  function createBoxes(amount) {
-    destroyBoxes();
-    let size =30;
-    for (let i = 0; i < amount; i++) {
-      const box = document.createElement(`div`);
-      box.style.width = `${size}px`;
-      box.style.height = `${size}px`;
-      box.style.backgroundColor = getRandomHexColor();
-      boxesContainer.appendChild(box);
-      size += 10;
-    }
-  }
-  function destroyBoxes() {
-    boxesContainer.innerHTML = ``;
-  }
-}
-);
+});
+destroyBtn.addEventListener(`cick`, () => {
+  destroyBoxes();
+
+})
